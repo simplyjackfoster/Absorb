f12f050b40451bcc8e8dd4887cb0d5ebe66b893c
Jack Foster <jackfoster246@gmail.com>
2022-12-26 11:39:28 -0500

    User schema

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,7 @@
+const mongoose = require('mongoose');
 let userSchema = mongoose.Schema({
     email: { type: String, unique: true },
     name: { type: String },
     password: { type: String },
 }, { timestamps: true });
+module.exports = mongoose.model('User', userSchema);
